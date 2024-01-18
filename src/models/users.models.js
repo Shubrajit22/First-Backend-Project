d1205bf0d550f0e89f3d839292cfd9913df5090e
[shubro@gm] <shubrajitdeb180603@gmail.com>
2024-01-18 21:47:08 +0530

    jwt initiated

--- src/models/users.models.js
+++ src/models/users.models.js
@@ -68,4 +68,24 @@ userSchema.methods.isPasswordCoorect= async function (password)
 {
    return await bcrypt.compare(password,this.password)
 }
+//jwt has a method known as sign() which generates tokens we need to give them payload(data),etc
+userSchema.methods.generateAccessToken= function ()
+{
+     return jwt.sign(
+      {
+         _id:this._id,
+         email:this.email,
+         username:this.userName,
+         fullname:this.fullName
+      },
+      process.env.ACCESS_TOKEN_SECRET,
+      {
+         expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+      }
+     )
+}
+userSchema.methods.generateRefreshToken= function ()
+{
+   
+}
 export const User = mongoose.model("User", userSchema)
