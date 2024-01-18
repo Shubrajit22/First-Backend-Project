0e142f9408d0dca8118d8f2bd9e0bd829b53cdba
[shubro@gm] <shubrajitdeb180603@gmail.com>
2024-01-18 21:33:59 +0530

    video models including user models password hashing using bcrypt

--- src/models/users.models.js
+++ src/models/users.models.js
@@ -1,59 +1,71 @@
-import mongoose,{Schema} from "mongoose";
-const userSchema = new Schema({
-   userName:{
-    type:String,
-    required:true,
-    unique:true,
-    lowercase:true,
-    trim:true,
-    index:true
-   },
-    email:{
-    type:String,
-    required:true,
-    unique:true,
-    lowercase:true,
-    trim:true,
-    // index:true
-   },
-    fullName:{
-    type:String,
-    required:true,
-    // unique:true,
-    // lowercase:true,
-    trim:true,
-    index:true
-   },
-   avatar:{
-    type:String,
-    required:true
-   },
-   coverImage:{
-    type:String,
-   },
-   watchHistory:[{
-    type:Schema.Types.ObjectId,
-    ref:"Video"
-   }],
-   password:{
-    type:String,
-    required:[true,'Password is required'],
-   },
-   refreshToken:{
-    type:String
-   }
-},
-   {
-    timestamps:true
-   }
-   
-   
-   
-)
+import mongoose, { Schema } from "mongoose";
+import { Jwt } from "jsonwebtoken";
+import bcrypt from "bcrypt";
+const userSchema = new Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      // index:true
+    },
+    fullName: {
+      type: String,
+      required: true,
+      // unique:true,
+      // lowercase:true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String,
+      required: true,
+    },
+    coverImage: {
+      type: String,
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-
-
-
-
-
-export const User = mongoose.model("User",userSchema);
+//bcrypt package has been used to hash the password in the data base.
+//we are using if statement to check whether the password has been modified or not ,this helps us not encrypt the password again and again once password has been modified then only hashing will take place or not
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = bcrypt.hash(this.password, 10)
+  next()
+})
+//we can create our own methods()'
+//bcrypt also provides us the functionality to check passwords
+//it takes the password given by the user and compares with its encrypted password(this.password)
+//return true (password matches) or false
+userSchema.methods.isPasswordCoorect= async function (password)
+{
+   return await bcrypt.compare(password,this.password)
+}
+export const User = mongoose.model("User", userSchema)
