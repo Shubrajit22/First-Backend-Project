a1a5b047a8f412ca9cd80d051f59563aed7afe14
[Shubrajit] <shubrajitdeb180603@gmail.com>
2024-01-24 15:09:15 +0530

    second commit

---
+++ src/controller/user.controller.js
@@ -0,0 +1,14 @@
+import express from "express"
+ const app = express();
+
+
+
+
+
+
+
+
+
+
+
+ export { app }
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import dotenv from "dotenv";
+
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path: "./env",
+});
+
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(`server is running at port :${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGODB CONNECTION FAILED !!!", err);
+  });
